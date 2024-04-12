8e5fc5df842e3f8dd722de1f796e1cbe31eb3d26
Abhishek Kumar <abhishek.kum1908@gmail.com>
2024-04-12 15:49:14 +0530

    First push

---
+++ android/app/src/main/java/com/rnsdk/ReactNativeActivity.java
@@ -0,0 +1,10 @@
+package com.rnsdk;
+
+import com.facebook.react.ReactActivity;
+
+public class ReactNativeActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "RNSDK";
+    }
+}
